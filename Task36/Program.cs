e7f69a2496443ba19180a77a5889d358867f392b
Kseniia <ksenjadzjubina@gmail.com>
2022-07-28 15:56:57 +0300

    Задача 36

---
+++ Task36/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("\nМассив:   ");
+    int [] Array2 = new int[15];
+Random range2 = new Random();
+    Console.Write("[");
+    for (int i = 0; i < Array2.Length; i++)
+         {
+            Array2[i] = range2.Next(-50, 99);
+            Console.Write(Array2[i] + " ");
+         }
+    Console.Write("]");
+
+int SumOfOddNumbers = 0;
+    for (int i = 0; i < Array2.Length; i ++)
+        {
+           if (i % 2 != 0)
+           {
+            SumOfOddNumbers = SumOfOddNumbers + Array2[i];
+           }
+        }    
+  
+Console.WriteLine($"\nСумма элементов массива на нечётных позициях равна: {SumOfOddNumbers}");
