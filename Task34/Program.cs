656e1dc5ab5dfe7dda36cfa959a7120934787621
Kseniia <ksenjadzjubina@gmail.com>
2022-07-28 15:51:27 +0300

    Задача 34

---
+++ Task34/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Массив:   ");
+    int [] Array = new int[5];
+Random range = new Random();
+
+    for (int i = 0; i < Array.Length; i++)
+         {
+            Array[i] = range.Next(100, 999);
+            Console.Write(Array[i] + ",  ");
+         }
+
+int EvenNumber = 0;
+    for (int i = 0; i < Array.Length; i ++)
+        {
+           if (Array[i] % 2 == 0) EvenNumber++;
+        }
+
+
+Console.Write($"\nКоличество чётных элементов в массиве равно: {EvenNumber}" );
+
